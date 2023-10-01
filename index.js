970daddf64421e769e4aeff3a77b99082c103a04
EmSturm <emsturmova@gmail.com>
2023-10-01 18:23:25 +0200

    Vypracované cvičení

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const goodbye = (name) => {
   return 'S pozdravem ' + name;
 };
 
+const formalGoodbye = (name) => {
+  return 'S uctivoou poklonou' + name;
+}
+
+const rudeGoodbye = (name) => {
+  return 'Se měj' + name;
+}
+
 const fillSubject = (subject) => {
   document.querySelector('.email__subject').textContent = subject;
 };
 
-const fillBody = (body, name) => {
+const fillBody = (body, name, goodbyeFunction) => {
   const bodyElm = document.querySelector('.email__body');
-  bodyElm.innerHTML += body;
+  bodyElm.innerHTML = body;
   const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  closingElm.textContent = goodbyeFunction(name);
 };
+
+document.body.innerHTML = fillSubject('Obchodní sdělení')
+
+document.body.innerHTML = fillBody('Píši vám ohledně', 'Ema Šturmová', formalGoodbye)
